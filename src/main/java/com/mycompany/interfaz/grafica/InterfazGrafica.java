4439113b76ceef837d7f01a4299028789de3d5bf
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-04-03 19:16:24 -0300

    interfaz de usuario

---
+++ src/main/java/com/mycompany/interfaz/grafica/InterfazGrafica.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfaz.grafica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author Pablo
+ */
+public class InterfazGrafica {
+
+    public static void main(String[] args) {
+        Pantalla pantalla=new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
